fbc7df06258a8068587d806e22d9060c6060f62c
bntufte <bntufte@outlook.com>
2020-12-12 11:49:39 -0800

    progress update

---
+++ src/main/java/com/revature/web/MasterServlet.java
@@ -0,0 +1,28 @@
+package com.revature.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MasterServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2854068448754481397L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+}
